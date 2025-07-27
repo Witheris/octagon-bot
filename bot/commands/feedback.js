7f0c2b984496d26448701b6e122a8fc7283a6d43
Witheris <grigoriy.kozlov.04@mail.ru>
2025-07-27 23:52:24 +0800

    Delete bot/commands/feedback.js

--- bot/commands/feedback.js
+++
@@ -1,7 +0,0 @@
-const feedbackHandler = require('../handlers/feedbackHandler');
-
-module.exports = (bot) => {
-  bot.onText(/Обратная связь/, async (msg) => {
-    await feedbackHandler.showFeedbackMenu(bot, msg);
-  });
-};
